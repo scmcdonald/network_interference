ee638df37b6fcdc616698f02799e29bda652bfa0
Sarah McDonald <scmcd@stanford.edu>
2022-05-24 14:13:03 -0700

    sh tests

---
+++ bigvar_test.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+
+df <- data.frame(x = c(1,2,3), y = c(2,3,4))
+
+print(df)
---
+++ run_bigvar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=run_bigvar
+#
+#SBATCH --output=out/run_bigvar_%j.out
+#SBATCH --time=60
+#SBATCH --mem=8G
+#SBATCH --mail-type=all
+#SBATCH --mail-user=scmcd@stanford.edu
+
+Rscript bigvar_test.R
